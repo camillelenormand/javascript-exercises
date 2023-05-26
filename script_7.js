17b20166c9bef5e64a95b89394b207795dd00914
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-26 16:18:10 +0200

    add new comments

--- script_7.js
+++ script_7.js
@@ -2,6 +2,15 @@
 
 // Chatbot
 
+// Create a chatbot that will answer questions from the user.
+// The chatbot will be able to answer 4 types of questions:
+// - Questions ending with "?": "Yes. Of course!"
+// - Questions without "?": "Are you okay?"
+// - Questions in all caps: "Calm down..."
+// - Questions containing the word "Fortnite": "Do you want to play Fortnite with me?"
+// - All other questions: "I don't understand..."
+
+// Style the page
 document.body.style.background = "rgb(238,174,202)";
 document.body.style.background = "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)";
 document.body.style.color = "white";
@@ -9,15 +18,25 @@ document.body.style.fontFamily = "monospace";
 document.body.style.fontSize = "1.5rem";
 document.body.style.textAlign = "center";
 
+// Create the page content
 document.body.innerHTML = `
   <h1>Chatbot</h1>
   <p>Ask me a question!</p>
 `;
 
+
+// Create the input field and the button and the answer field
 const input = document.createElement("input");
 const button = document.createElement("button");
+const answer = document.createElement("p");
+let question = input.value;
+
+// Append the elements to the page
+document.body.append(input, button, answer);
 
 
+
+// Style the elements
 input.style.width = "40%";
 input.style.height = "2rem";
 input.style.fontSize = "1.5rem";
@@ -50,32 +69,6 @@ button.style.textTransform = "uppercase";
 button.style.letterSpacing = "0.1rem";
 button.style.transition = "all 0.3s ease-in-out";
 
-button.addEventListener("mouseover", () => {
-  button.style.backgroundColor = "white";
-  button.style.color = "black";
-});
-
-button.addEventListener("mouseout", () => {
-  button.style.backgroundColor = "black";
-  button.style.color = "white";
-
-});
-
-button.addEventListener("click", () => {
-  button.style.backgroundColor = "black";
-  button.style.color = "white";
-});
-
-
-
-
-document.body.append(input, button);
-
-let question = input.value;
-let answer = document.createElement("p");
-answer.placeholder = "Bot's answer";
-answer.readOnly = true;
-document.body.append(answer);
 
 answer.style.fontSize = "1.5rem";
 answer.style.fontFamily = "monospace";
@@ -89,19 +82,41 @@ answer.style.color = "black";
 answer.style.transition = "all 0.3s ease-in-out";
 answer.style.fontWeight = "bold";
 
+// Add an event listener to the button for mouseover
+button.addEventListener("mouseover", () => {
+  button.style.backgroundColor = "white";
+  button.style.color = "black";
+});
 
+// Add an event listener to the button for mouseout
+button.addEventListener("mouseout", () => {
+  button.style.backgroundColor = "black";
+  button.style.color = "white";
+});
 
+// Add an event listener to the button for click (to reset the button style)
+button.addEventListener("click", () => {
+  button.style.backgroundColor = "black";
+  button.style.color = "white";
+});
+
+// Add an event listener to the button for click
 button.addEventListener("click", () => {
   question = input.value;
 
+  // If the question ends with "?"
   if (question.endsWith("?")) {
     answer.textContent = "Yeah, sure...";
+    // If the question is empty
   } else if (!question) {
     answer.textContent = "Are you okay?";
+    // If the question is in all caps
   } else if (question === question.toUpperCase()) {
     answer.textContent = "Calm down...";
+    // If the question contains the word "Fortnite"
   } else if (question.includes("Fortnite")) {
     answer.textContent = "Do you want to play Fortnite with me?";
+    // If the question is anything else
   } else {
     answer.textContent = "I don't understand...";
   }
